e6b01192b4c55edce34be33ccf879f5aedbbe289
YARRACHANDU <yarrachandu7@gmail.com>
2026-03-22 15:29:26 +0530

    hi

--- backend-ProMax/server.js
+++ backend-ProMax/server.js
@@ -1,22 +1,22 @@
-    const express = require("express");
-    const mongoose = require("mongoose");
-    const cors = require("cors");
-    require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-    const app = express();
+const app = express();
 
-    app.use(cors());
-    app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-    mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected ✅"))
-    .catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log(err));
 
-    app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/authRoutes"));
 
-    const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
